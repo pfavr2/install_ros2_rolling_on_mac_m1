20a590d85902132385f9898faef645ed20ea15e9
Peter Favrholdt <ros2github@how.dk>
2023-12-21 15:14:00 +0100

    Work in progress - most packages build now on M1

--- install.sh
+++ install.sh
@@ -128,7 +128,8 @@ patch < "$SCRIPT_DIR/patches/ros2_rviz_default_plugins_include_directories_qt5.1
 patch < "$SCRIPT_DIR/patches/ros2_rviz_common_include_directories_qt5.15.10.patch"
 patch < "$SCRIPT_DIR/patches/ros2_mimic_vendor_vcs_version.patch"
 patch < "$SCRIPT_DIR/patches/ros2_rviz2_include_directories_qt5.15.10.patch"
-
+patch < "$SCRIPT_DIR/patches/ros2_kdl_parser_orocos-kdl_include_directories.patch"
+patch < "$SCRIPT_DIR/patches/ros2_rosbag2_transport_uint64_t.patch"
 colcon build --symlink-install --cmake-args -DBUILD_TESTING=OFF -Wno-dev --packages-skip-by-dep python_qt_binding
 
 #colcon build \
